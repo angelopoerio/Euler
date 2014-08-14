c0b0114b5c742abc558d9a55162473a573d4f7ea
angelopoerio89 <angelo_poerio>
2014-08-14 16:57:32 +0200

    This repository contains my solutions for http://projecteuler.net/ challenges

--- 48/solution.py
+++ 48/solution.py
@@ -2,8 +2,7 @@
 # Author: Angelo Poerio <angelo.poerio@gmail.com>
 
 sum = 0
-print "Computing..."
 for i in range(1, 1000 + 1):
 	sum += i**i
 
-print "Answer is {0}".format(str(sum)[-10:])
+print "Answer is {0}".format(str(sum)[-10:]) # 9110846700
